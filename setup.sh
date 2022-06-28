7a5f709375dc2ede71f8512766116fbb3e30de4e
SiriusKoan <k.peihsun@gmail.com>
2022-06-28 04:40:00 +0000

    Write settings to server.properties

--- setup.sh
+++ setup.sh
@@ -45,12 +45,17 @@ fi
 
 # install necessary packages
 printTitle "Install Necessary Packages"
-echo "Package update..."
-apt update
-echo "Install Docker..."
-apt install docker.io
-echo "Install wget..."
-apt install wget
+if [ $test ]
+then
+    echo "Testing, skipped."
+else
+    echo "Package update..."
+    apt update
+    echo "Install Docker..."
+    apt install docker.io
+    echo "Install wget..."
+    apt install wget
+fi
 
 # Check configs
 printTitle "Check configs"
@@ -61,21 +66,54 @@ echo "$count config files found."
 
 # generate docker-compose.yaml and download server.jar
 printTitle "Generate docker-compose.yaml and download server.jar"
+> rcon.txt
 for f in "${files[@]}"
 do
     # setup docker
     ver=$(cat "config/$f" | grep version | awk -F= '{print $2; exit;}')
     download_link=$(cat versions.txt | grep "$ver " | awk '{print $2}')
-    server_name=$(echo ${f%.*})
+    server_name=${f%.*}
     port=$(cat "config/$f" | grep port | awk -F= '{print $2; exit;}')
-    server=$(cat src/docker/docker-compose-template.yaml | sed -e "s/%%SERVERNAME%%/$server_name/" -e "s/%%PORT%%/$port/")
+    server=$(cat src/docker/docker-compose-template.yaml | sed -e "s/%%SERVERNAME%%/$server_name/" -e "s/%%PORT%%/$port/" -e "s/%%RCONPORT%%/$(expr $port + 1000)/")
     servers+="\n$server"
     # setup basic file
     mkdir -p data/${server_name}
     wget ${download_link} -O "./data/${server_name}/server.jar"
+    echo "${server_name}:$(expr $port + 1000)" >> rcon.txt
+    # check firewall
+    if [ $firewall ]
+    then
+        echo "Firewall is enabled, allow traffic to $port"
+        ufw allow to any port $port
+    fi
 done
 echo "version: \"2\"" > docker-compose.yaml
 echo "services:" >> docker-compose.yaml
 echo -e "$servers" >> docker-compose.yaml
 printOK "Done."
-echo "Please use docker-compose up -d to start the servers."
+
+# generate server files by running docker-compose
+printTitle "Generate necessary server files"
+docker-compose up -d
+printOK "Done"
+
+# sign eula
+printTitle "Sign EULA"
+for f in "${files[@]}"
+do
+    sed -i "s/false/true/" data/${f%.*}/eula.txt
+done
+printOK "Done"
+
+# write settings
+printTitle "Write settings to servers"
+for f in "${files[@]}"
+do
+    conf=$(cat config/$f | grep -v version)
+    echo "$conf" >> data/${f%.*}/server.properties
+    # enable rcon
+    echo "rcon-enable=true" >> data/${f%.*}/server.properties
+    echo "rcon-password=123456" >> data/${f%.*}/server.properties
+done
+printOK "Done"
+
