665b856696f6c382eca2c3a5a0a7ed8c944f7852
SiriusKoan <k.peihsun@gmail.com>
2022-07-01 21:37:17 +0800

    Add copyright

---
+++ src/web/frontend/src/components/Copyright.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>&copy; <a href="https://blog.siriuskoan.one" target="_blank">SiriusKoan</a> 2022</p>
+</template>
+
+<style>
+a {
+    text-decoration: none;
+    color: var(--color-text);
+}
+</style>
