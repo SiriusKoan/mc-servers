8bcc21aa7c8074c62c75589c8552084bd5dd507c
SiriusKoan <k.peihsun@gmail.com>
2022-07-04 22:28:46 +0800

    feat(frontend): Add rcon support

--- src/web/frontend/src/App.vue
+++ src/web/frontend/src/App.vue
@@ -4,6 +4,8 @@ import Copyright from './components/Copyright.vue'
 export default {
     data() {
         return {
+            messages: {},
+            status: {},
             servers: null,
             //servers_info: [{"name": "1"}, {"name": "2"}, {"name": "3"}, {"name": "4"}, {"name": "5"}]
             servers_info: []
@@ -13,6 +15,23 @@ export default {
         Card,
         Copyright
     },
+    methods: {
+        sendCommand(server_name, command) {
+            console.log(server_name, command);
+            axios
+            .post(`/api/${server_name}`, {
+                'command': command
+            })
+            .then((res) => {
+                this.status[server_name] = 'success';
+                this.messages[server_name] = res['data'];
+            })
+            .catch((err) => {
+                this.status[server_name] = 'error';
+                this.messages[server_name] = err['response']['data'];
+            })
+        }
+    },
     mounted() {
         axios
         .get('/api/all')
@@ -21,7 +40,7 @@ export default {
             for (let i = 0; i < this.servers.length; i++) {
                 if (this.servers[i][1]) {
                     axios
-                    .get('/api/' + this.servers[i][0])
+                    .get(`/api/${this.servers[i][0]}`)
                     .then((res) => {
                         res['data']['status'] = 'active';
                         this.servers_info.push(res['data']);
@@ -42,7 +61,7 @@ export default {
 <template>
   <h1>Minecraft Servers</h1>
   <main>
-    <Card v-for="server in servers_info" :class="server.status" :status="server.status" :name="server.name" :players="server.players" :seed="server.seed" :max="server.max" :online="server.online" />
+      <Card v-for="server in servers_info" :class="server.status" v-bind="{ server }" :message="messages[server.name]" :status="status[server.name]" @send="sendCommand" />
   </main>
   <footer>
       <Copyright />
--- src/web/frontend/src/components/Card.vue
+++ src/web/frontend/src/components/Card.vue
@@ -3,24 +3,31 @@
         <div>
             <div class="label">
                 <span>Name: </span>
-                <span>{{ name }}</span>
+                <span>{{ server.name }}</span>
             </div>
             <div class="label">
                 <span>Status: </span>
-                <span>{{ status }}</span>
+                <span>{{ server.status }}</span>
             </div>
-            <div v-if="status === 'active'">
+            <div v-if="server.status === 'active'">
                 <div class="label">
                     <span>Seed: </span>
-                    <span>{{ seed }}</span>
+                    <span>{{ server.seed }}</span>
                 </div>
                 <div class="label">
                     <span>Status: </span>
-                    <span>{{ online }} / {{ max }}</span>
+                    <span>{{ server.online }} / {{ server.max }}</span>
                 </div>
                 <div class="label">
                     <span>Players: </span>
-                    <span v-for="player in players">{{ player }} </span>
+                    <span v-for="player in server.players">{{ player }} </span>
+                </div>
+                <div class="label">
+                    <input type="text" placeholder="Command" v-model="command">
+                    <button @click="sendCommandSignal">Send</button>
+                </div>
+                <div class="message" v-if="message" :class="status">
+                    {{ message }}
                 </div>
             </div>
         </div>
@@ -30,7 +37,17 @@
 <script>
 export default {
     name: 'Card',
-    props: ['status', 'name', 'players', 'seed', 'max', 'online']
+    props: {
+        server: null,
+        command: String,
+        message: String,
+        status: String
+    },
+    methods: {
+        sendCommandSignal() {
+            this.$emit('send', this.server.name, this.command);
+        }
+    }
 }
 </script>
 
@@ -41,8 +58,8 @@ export default {
     box-shadow: 0px 0px 6px 1px rgba(255,255,255,0.75);
     -webkit-box-shadow: 0px 0px 6px 1px rgba(255,255,255,0.75);
     -moz-box-shadow: 0px 0px 6px 1px rgba(255,255,255,0.75);
-    width: 300px;
-    height: 150px;
+    width: 330px;
+    height: 170px;
     border-radius: 7px;
     display: flex;
     flex-direction: column;
@@ -62,5 +79,21 @@ export default {
 .inactive {
     background-color: rgba(0, 0, 0, 0.6);
     opacity: 0.5;
+}
+
+.message {
+    color: white;
+    display: block;
+    margin: 10px auto;
+	padding: 20px;
+	box-shadow: 3px 2px 10px 1px rgba(0, 0, 0, 0.3);
+}
+
+.success {
+    background-color: #04AA6D;
+}
+
+.error {
+    background-color: #f44336;
 }
 </style>
